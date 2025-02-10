91eb90a07d3ff310580e9c537c93885352c9af96
aswinxd <musiqorobot@gmail.com>
2025-02-10 20:43:24 +0530

    ok

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-#(©)Codexbotz
-
 from aiohttp import web
 from plugins import web_server
 
@@ -41,15 +39,21 @@ class Bot(Client):
                         link = await self.export_chat_invite_link(chat_id=channel)
                     self.invite_links[channel] = link
                 except Exception as a:
-                    return
-                 
+                    self.LOGGER(__name__).warning(a)
+                    self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                    self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNELS value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNELS}")
+                    self.LOGGER(__name__).info("\nBot Stopped.")
+                    sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
             test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
             await test.delete()
         except Exception as e:
-            return
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped.")
+            sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!")
