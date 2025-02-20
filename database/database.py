aa9b82eeb17b0eb440c939787bb328325d382303
private <155294690+aswinxd@users.noreply.github.com>
2025-02-20 22:00:17 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -2,44 +2,35 @@ import pymongo, os
 from config import DB_URI, DB_NAME
 from pymongo.collection import Collection
 
+import motor.motor_asyncio
+from config import DB_URI, DB_NAME
 
-dbclient = pymongo.MongoClient(DB_URI)
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
-
 user_data = database['users']
-join_requests: Collection = database['join_request']
+join_requests = database['join_request']
 
-
-async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
+async def present_user(user_id: int):
+    found = await user_data.find_one({'_id': user_id})
     return bool(found)
 
+
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
-    return
+    await user_data.insert_one({'_id': user_id})
+
 
 async def full_userbase():
     user_docs = user_data.find()
-    user_ids = []
-    for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
+    user_ids = [doc['_id'] async for doc in user_docs]  # Use async iteration
     return user_ids
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
-    return
+    await user_data.delete_one({'_id': user_id})
 
-async def add_join_request(group_id: int, user_id: int) -> None:
-    join_requests.insert_one({
-        'group_id': group_id,
-        'user_id': user_id,
-    })
-
+async def add_join_request(group_id: int, user_id: int):
+    await join_requests.insert_one({'group_id': group_id, 'user_id': user_id})
+    
 async def check_join_request(group_id: int, user_id: int) -> bool:
-    request = join_requests.find_one({
-        'group_id': group_id,
-        'user_id': user_id
-    })
+    request = await join_requests.find_one({'group_id': group_id, 'user_id': user_id})
     return bool(request)
